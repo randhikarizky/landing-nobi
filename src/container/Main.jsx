15f05a89fb779177da309f19d28fac2aee6c4218
rizkyran <rizkyran.rr@gmail.com>
2021-11-27 01:02:23 +0700

    add some styling

--- src/container/Main.jsx
+++ src/container/Main.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
-import '../components/style/main.css'
+import '../components/style/main.scss'
+import ArtRocket from '../components/assets/img/art-rocket.svg'
+import AppBtn from '../components/assets/img/btn-appstore.svg'
+import PlayBtn from '../components/assets/img/btn-playstore.svg'
 
 export class Main extends Component {
     render() {
@@ -55,12 +58,17 @@ export class Main extends Component {
                                     Automate your crypto trading strategy and take your emotion out of the equation
                                 </p>
                                 <div className="btn-container">
-
+                                    <a href="#">
+                                        <img src={AppBtn} alt="" />
+                                    </a>
+                                    <a href="#">
+                                        <img src={PlayBtn} alt="" />
+                                    </a>
                                 </div>
                             </div>
                             <div className="col-md">
                                 <div className="img-container">
-
+                                    <img src={ArtRocket} alt="" />
                                 </div>
                             </div>
                         </div>
@@ -68,28 +76,99 @@ export class Main extends Component {
                 </div>
                 <div className="container-fluid" id="section-2">
                     <div className="row">
-                        <div className="tab-container">
-                            <div className="btn-group" role="group" >
-                                <input type="radio" className="btn-check" checked />
-                                <label class ="btn btn-outline-primary" >Strategy</label>
+                        <div className="col-md">
+                            <div className="tab-container">
+                                <div className="btn-group" role="group" >
+                                    <input type="radio" className="btn-check" checked />
+                                    <label class="btn btn-outline-primary" >Strategy</label>
 
-                                <input type ="radio" class ="btn-check" />
-                                <label class ="btn btn-outline-primary" >Stalking</label>
+                                    <input type="radio" class="btn-check" />
+                                    <label class="btn btn-outline-primary" >Stalking</label>
 
-                                <input type ="radio" class ="btn-check" />
-                                <label class ="btn btn-outline-primary" >Saving</label>
+                                    <input type="radio" class="btn-check" />
+                                    <label class="btn btn-outline-primary" >Saving</label>
+                                </div>
                             </div>
-                        </div>
-                        <div className="table-container">
-
-                        </div>
-                        <div className="btn-container">
-                            <button className="btn btn-primary btn-arrow">
-                                Explore More
-                                <span>
-
-                                </span>
-                            </button>
+                            <div className="table-container">
+                                <table className="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Strategy</th>
+                                            <th scope="col">Marketcap</th>
+                                            <th scope="col">Volume</th>
+                                            <th scope="col">Est. Annual Yield</th>
+                                            <th scope="col">Node Collateral</th>
+                                            <th scope="col">Hosted Node</th>
+                                            <th scope="col" colspan="3">1 Month</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">DASH</th>
+                                            <td>Nova Kapital</td>
+                                            <td>$148.574.33</td>
+                                            <td>-12.45%</td>
+                                            <td>-2.12%</td>
+                                            <td>-212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">SMART</th>
+                                            <td>Nova Kapital</td>
+                                            <td>$148.574.33</td>
+                                            <td>-2.36%</td>
+                                            <td>-1.45%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">ZCOIN</th>
+                                            <td>Nova Kapital</td>
+                                            <td>$148.574.33</td>
+                                            <td>-1.66%</td>
+                                            <td>-1.21%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Nova Obscura</th>
+                                            <td>Nova Kapital</td>
+                                            <td>$148.574.33</td>
+                                            <td>-7.50%</td>
+                                            <td>-2.22%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">New Oscillator</th>
+                                            <td>Nova Kapital</td>
+                                            <td>$148.574.33</td>
+                                            <td>-8.92%</td>
+                                            <td>-3.23%</td>
+                                            <td>-212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                            <td>+212.3%</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="btn-container">
+                                <button className="btn btn-primary btn-arrow">
+                                    Explore More
+                                    <span>
+                                        <i className="fas fa-arrow-right"></i>
+                                    </span>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
